21589a1459d079ba1b1ca3672dec71bca1b7980e
VISTALL <vistall.valeriy@gmail.com>
2014-10-21 02:29:12 +0400

    support set config file

--- Program.cs
+++ Program.cs
@@ -15,10 +15,26 @@ class ProxyObject : MarshalByRefObject
     public void LoadAndCall(String path, String type, String method, Object[] parameters)
     {
         Assembly assembly = Load(path);
+        if (assembly == null)
+        {
+            Console.WriteLine("Assembly {0} is null", path);
+            return;
+        }
 
         Type t = assembly.GetType(type);
+        if (t == null)
+        {
+            Console.WriteLine("Type {0} is null. Assembly {1}", type, assembly);
+            return;
+        }
 
         MethodInfo methodInfo = t.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic, null, new Type[] { typeof(String[]) }, null);
+
+        if (methodInfo == null)
+        {
+            Console.WriteLine("Method 'Main' is not found. Type {0}", type);
+            return;
+        }
 
         methodInfo.Invoke(null, parameters);
     }
@@ -30,12 +46,25 @@ namespace loader
     {
         static void Main(string[] args)
         {
-            String mainExecutable = args[0];
+            String mainExecutableAndConfig = args[0];
             String basePath = args[1];
             String typeName = args[2];
 
+            String mainExecutable = mainExecutableAndConfig;
+            String configFile = null;
+            if (mainExecutableAndConfig.Contains(";"))
+            {
+                string[] split = mainExecutableAndConfig.Split(';');
+                mainExecutable = split[0];
+                configFile = split[1];
+            }
+
             AppDomainSetup setup = new AppDomainSetup();
             setup.ApplicationBase = basePath;
+            if (configFile != null)
+            {
+                setup.ConfigurationFile = configFile;
+            }
             AppDomain domain = AppDomain.CreateDomain("ProxyDomain", null, setup);
 
             ProxyObject proxyObject = (ProxyObject)domain.CreateInstanceFromAndUnwrap(typeof(ProxyObject).Assembly.Location, "ProxyObject");
